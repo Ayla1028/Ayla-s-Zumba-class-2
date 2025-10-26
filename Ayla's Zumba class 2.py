b7ead816a42bc711853cc5b1452d524adc231412
Ayla1028 <Khalife1002@gmail.com>
2025-10-26 16:32:01 +0200

    Update Ayla's Zumba class 2.py

--- Ayla's Zumba class 2.py
+++ Ayla's Zumba class 2.py
@@ -11,22 +11,44 @@ while True:
     if action == "view participants":
         print ("\033[1mParticipants:\033[0m")
         for participant in participants:
-            print("-",{participant["name"]})
+            print("-",{participant["Name"]})
         
     if action == ("view details").strip().casefold():
         for participant in participants:
-            print(f"\033[1m name:\033[0m{participant['Name']},\033[1m Age:\033[0m {participant['age']}, \033[1m Genre:\033[0m {participant['genre']}, \033[1m Niveau:\033[0m {participant['niveau']}, \033[1m Dance:\033[0m {participant['dance']}")
+            print(f"\033[1m Name:\033[0m{participant['Name']},\033[1m Age:\033[0m {participant['Age']}, \033[1m Genre:\033[0m {participant['Genre']}, \033[1m Niveau:\033[0m {participant['Niveau']}, \033[1m Dance:\033[0m {participant['Dance']}")
         
             
     if action == ("exit").strip().casefold():
         print ("Participant modifications are over!")
         break
     
-    if action == ("view specific details").strip().casefold():
+    if action == ("view participant details").strip().casefold():
         participant_entered = input("The details of which participant would you like to view? ")
         for participant in participants:
             if participant['Name'] == participant_entered:
-                
-                print(f"\033[1mName:\033[0m {participant['Name']},\033[1mAge:\033[0m {participant['age']},\033[1m Genre:\033[0m {participant['genre']},\033[1m Niveau:\033[0m {participant['niveau']},\033[1m Dance:\033[0m {participant['dance']}")
+                 print(f"\033[1mName:\033[0m {participant['Name']},\033[1mAge:\033[0m {participant['Age']},\033[1m Genre:\033[0m {participant['Genre']},\033[1m Niveau:\033[0m {participant['Niveau']},\033[1m Dance:\033[0m {participant['Dance']}")
+                 
+    if action == ("clear").strip().casefold():
+        participants.clear()
+        print("The list of participants has been cleared.")
+        
+    if action == ("add participant").strip().casefold():
+        participant_name = input ("Name of the new participant: ").strip().casefold()
+        if participant_name.title() in participants:
+            print(participant_name.title(), "is already a participant")
+            answer = input ("Do you still want to add a new participant?").strip().casfold()
+            if answer == "yes":
+                participant_name = input ("Name of the new participant: ").strip().casefold()
+            elif answer == "no":
+                print ("Okay, no problem!")
+        
+        participant_age = input ("Age of new participant: ").strip().casefold()
+        participant_genre = input ("Gender of new participant: ").strip().casefold()
+        participant_niveau = input("Level of new participant: ").strip().casefold()
+        participant_dance = input ("Dance type of new participant: ").strip().casefold()
+        
+        
+        
+        
 
     
